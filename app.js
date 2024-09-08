c71ec91e189709347914d5ea69014e2c07b45edc
saurabh <ysaurabh2003@gmail.com>
2024-09-08 13:32:41 +0530

    Flage change done

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
-let Data = "users data"
-class Users{
-    constructor(name, email){
-        this.name = name;
-        this.email = email;
+const BASE_URL="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json"
+
+const dropdownSelect = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button")
+
+for(let select of dropdownSelect){
+    for(currencyCode in countryList){
+        // console.log(currencyCode, countryList[currencyCode])
+        let option = document.createElement("option");
+        option.innerText=currencyCode;
+        option.value = currencyCode;
+        if (select.name === "from" && currencyCode === "USD"){
+            option.selected = true;
+        } else if (select.name === "to" && currencyCode === "INR"){
+            option.selected = true;
+        }
+        select.append(option);
     }
 
-    viewData(){
-        console.log("data = ", Data)
-    }
+    select.addEventListener("change", (evt) =>{
+        updateFlag(evt.target);
+    })
 }
 
-class Admin extends Users{
-    constructor(name, email){
-        super(name, email)
-    }
-editData(){
-        Data = "changed users data"
-        console.log(Data)
-    }
+const updateFlag = (element) => {
+    let currencyCode = element.value;
+    let countryCode = countryList[currencyCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img")
+    img.src = newSrc;
 }
 
-let student1 = new Users("saurabh", "abc@gamil.com")
-let student2 = new Users("gaurav", "xyz@gamil.com")
-
-let teacher1 = new Users("Teacher", "zyx@gmail.com")
-
-let admin1 = new Admin("admin ", "admin@gmail.com")
+btn.addEventListener("click", (evt) =>{
+    evt.preventDefault();
+})
